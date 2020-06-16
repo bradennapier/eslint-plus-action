79169972dbc57a214843a0291f40ce39261effa4
Braden Napier <brady.napier@dashos.net>
2020-06-16 12:47:23 -0400

    try docker method

---
+++ src/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+cd "${2:-.}" || echo "source root not found"
+
+[ -f yarn.lock ] && yarn install
+[ -f package-lock.json ] && npm install
+
+NODE_PATH=node_modules GITHUB_TOKEN="${GITHUB_TOKEN:-${1:-.}}" SOURCE_ROOT=${2:-.} node /action/lib/run.js
+
+rm -rf node_modules # cleanup to prevent some weird permission errors later on 
