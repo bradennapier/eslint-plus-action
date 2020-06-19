31105d126e1986e1a09a7379b45b8f70687fe413
Braden Napier <brady.napier@dashos.net>
2020-06-19 01:15:57 -0400

    chore: dont run tests for now

--- src/run.ts
+++ src/run.ts
@@ -6,52 +6,58 @@ import { processArrayInput, processBooleanInput, processInput } from './utils';
 import { ActionData } from './types';
 
 async function run(): Promise<void> {
-  const { context } = github;
+  try {
+    const { context } = github;
 
-  core.debug('👋 Hello! You are an amazing person! 🙌');
+    core.debug('👋 Hello! You are an amazing person! 🙌');
 
-  const client = github.getOctokit(
-    core.getInput('github-token', { required: true }),
-  );
+    const client = github.getOctokit(
+      core.getInput('github-token', { required: true }),
+    );
 
-  const data: ActionData = {
-    prID: github.context.payload.pull_request?.number,
-    sha: context.payload.pull_request?.head.sha || context.sha,
+    const data: ActionData = {
+      prID: github.context.payload.pull_request?.number,
+      sha: context.payload.pull_request?.head.sha || context.sha,
 
-    includeGlob: processArrayInput('includeGlob', []),
-    ignoreGlob: processArrayInput('ignoreGlob', []),
+      includeGlob: processArrayInput('includeGlob', []),
+      ignoreGlob: processArrayInput('ignoreGlob', []),
 
-    eslint: {
-      errorOnUnmatchedPattern: processBooleanInput(
-        'errorOnUnmatchedPattern',
-        false,
-      ),
-      extensions: processArrayInput('extensions', [
-        '.js',
-        '.jsx',
-        '.ts',
-        '.tsx',
-      ]),
-      rulePaths: processArrayInput('rulePaths', []),
-      followSymbolicLinks: processBooleanInput('followSymbolicLinks', true),
-      useEslintIgnore: processBooleanInput('useEslintIgnore', true),
-      ignorePath: processInput('ignorePath', null) || undefined,
-      useEslintrc: processBooleanInput('useEslintrc', true),
-      overrideConfigFile: processInput('overrideConfigFile', null) || undefined,
-      fix: processBooleanInput('useEslintrc', false),
-      fixTypes:
-        (processArrayInput('fixTypes', null) as (
-          | 'problem'
-          | 'suggestion'
-          | 'layout'
-          | undefined
-        )[]) || undefined,
-    },
-  };
+      eslint: {
+        errorOnUnmatchedPattern: processBooleanInput(
+          'errorOnUnmatchedPattern',
+          false,
+        ),
+        extensions: processArrayInput('extensions', [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ]),
+        rulePaths: processArrayInput('rulePaths', []),
+        followSymbolicLinks: processBooleanInput('followSymbolicLinks', true),
+        useEslintIgnore: processBooleanInput('useEslintIgnore', true),
+        ignorePath: processInput('ignorePath', null) || undefined,
+        useEslintrc: processBooleanInput('useEslintrc', true),
+        overrideConfigFile:
+          processInput('overrideConfigFile', null) || undefined,
+        fix: processBooleanInput('useEslintrc', false),
+        fixTypes:
+          (processArrayInput('fixTypes', null) as (
+            | 'problem'
+            | 'suggestion'
+            | 'layout'
+            | undefined
+          )[]) || undefined,
+      },
+    };
 
-  core.info(`Context:\n ${JSON.stringify(data, null, 2)}`);
+    core.info(`Context:\n ${JSON.stringify(data, null, 2)}`);
 
-  await lintChangedFiles(client, data);
+    await lintChangedFiles(client, data);
+  } catch (err) {
+    core.error(err);
+    core.setFailed(err.message);
+  }
 }
 
 run();
