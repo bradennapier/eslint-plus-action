e607f8d22abf405b6884dfb4c5e1644f8cae430d
Braden Napier <brady.napier@dashos.net>
2020-07-03 07:19:15 -0400

    skip serializer to test when test request

--- src/utils/SerializerOctokitPlugin/plugin.ts
+++ src/utils/SerializerOctokitPlugin/plugin.ts
@@ -3,6 +3,8 @@ import { OctokitPlugin, OctokitRequestOptions } from '../../types';
 import { requestRouteMatcher } from './routeMatcher';
 import { Serializers } from './serialize';
 
+const TempCache = new Set();
+
 export const SerializerOctokitPlugin: OctokitPlugin = (
   octokit: Parameters<OctokitPlugin>[0],
   clientOptions: Parameters<OctokitPlugin>[1],
@@ -23,18 +25,28 @@ export const SerializerOctokitPlugin: OctokitPlugin = (
       requestOptions: OctokitRequestOptions,
     ): Promise<unknown> => {
       if (!match || match.test(requestOptions.url)) {
+        const serialized = JSON.stringify({
+          ...requestOptions,
+          request: undefined,
+        });
+
+        const deserialized = JSON.parse(serialized);
+        if (TempCache.has(deserialized)) {
+          return request(requestOptions);
+        }
+
+        TempCache.add(deserialized);
+
         console.log('[SerializerOctokitPlugin] | Request | ', requestOptions);
+
         const serializer = Serializers.get(requestOptions.url);
         if (!serializer) {
           throw new Error(
             '[SerializerOctokitPlugin] | Attempted to serialize a path that is not handled',
           );
         }
-        const serialized = JSON.stringify({
-          ...requestOptions,
-          request: undefined,
-        });
-        console.log('Getting Result: ', JSON.parse(serialized));
+
+        console.log('Getting Result: ', deserialized);
         const result = await octokit.request(JSON.parse(serialized));
         console.log('RESULT: ', result);
         return result;
