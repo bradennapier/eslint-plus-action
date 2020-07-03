789ea4d66c69cef962fea5cb525290ee9db12aaa
Braden Napier <brady.napier@dashos.net>
2020-07-02 22:24:47 -0400

    run on labeled and review requested as test

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import * as github from '@actions/github';
+import { isPullRequestPayload } from './guards';
 
 // eslint-disable-next-line @typescript-eslint/prefer-namespace-keyword
 declare global {
@@ -10,30 +11,23 @@ declare global {
   }
 }
 
+const context = github.context;
+
 export const NAME = 'Eslint Changed';
 
-const C = {
-  event: '',
-};
+export const BASE_FULL_NAME = isPullRequestPayload(context.payload)
+  ? context.payload.pull_request.base.repo.full_name
+  : undefined;
 
-({
-  eventName: C.event,
-  issue: ,
-  payload: {
-    pull_request: {
-      base: {
-        repo: { full_name:  },
-      },
-      head: {
-        repo: { full_name: HEAD_FULL_NAME },
-      },
-    },
-  },
-} = github.context);
+export const HEAD_FULL_NAME = isPullRequestPayload(context.payload)
+  ? context.payload.pull_request.head.repo.full_name
+  : undefined;
 
-export const BASE_FULL_NAME = github.context.payload.pull_request?.additions
-
-export const { owner: OWNER, repo: REPO, number: ISSUE_NUMBER } = github.context.issue
+export const {
+  owner: OWNER,
+  repo: REPO,
+  number: ISSUE_NUMBER,
+} = github.context.issue;
 
 export const IS_READ_ONLY = BASE_FULL_NAME !== HEAD_FULL_NAME;
 
